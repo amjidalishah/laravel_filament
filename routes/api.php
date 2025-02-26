686fe1766bd3ca60f8e7c189b62568778a83bb14
amjid ali shah <amjidalishah72@gmail.com>
2025-02-26 18:33:18 +0500

    a

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\QR;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,19 +16,4 @@ use App\Models\QR;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
-
-Route::prefix('v1')->group(function () {
-    Route::post('/qr', function (Request $request) {
-        $qr = QR::create([
-            'title' => $request->title,
-            'target_domain' => $request->target_domain,
-            'origin_domain' => url("api/v1/" . str()->uuid()),
-        ]);
-        return response()->json($qr);
-    });
-    
-    Route::get('/qr/{id}', fn ($id) => response()->json(QR::findOrFail($id)));
-    Route::delete('/qr/{id}', fn ($id) => QR::destroy($id));
-    Route::get('/scan/{id}', fn ($id) => redirect(QR::findOrFail($id)->target_domain));
 });
